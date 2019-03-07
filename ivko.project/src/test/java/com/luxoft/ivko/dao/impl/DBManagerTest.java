43d1e73e53a0c97721223ffa3cf3c3cac122f666
ivko <sergii.ivko@gmail.com>
2019-03-07 10:40:39 +0200

    fix DB connection

--- ivko.project/src/test/java/com/luxoft/ivko/dao/impl/DBManagerTest.java
+++ ivko.project/src/test/java/com/luxoft/ivko/dao/impl/DBManagerTest.java
@@ -1,5 +1,7 @@
 package com.luxoft.ivko.dao.impl;
 
+import org.h2.jdbcx.JdbcDataSource;
+import org.h2.tools.Server;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -8,65 +10,35 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DBManagerTest {
-    Connection connection;
 
     @BeforeClass
     public static void init() throws SQLException {
         org.h2.tools.Server.createTcpServer().start();
-        DBManager.initialize(DBManager.getH2DataSource());
+        Server.createTcpServer("-tcpPort", "9082", "-tcpAllowOthers").start();
+        DBManager.initialize(DBManager.getTestH2DataSource());
     }
-
-//    @Before
-//    public void resetDb() {
-//        Connection connection;
-//        try {
-//            DeleteDbFiles.execute("~", "test", true);
-//            connection = DBManager.getConnection();
-//            Statement statement = connection.createStatement();
-//            statement.execute("DROP TABLE IF EXISTS client, product;");
-//            statement.execute("CREATE TABLE client (\n" +
-//                    "id INT PRIMARY KEY AUTO_INCREMENT,\n" +
-//                    "name VARCHAR(28),\n" +
-//                    "surname VARCHAR(28),\n" +
-//                    "email VARCHAR(28),\n" +
-//                    "password VARCHAR(28),\n" +
-//                    "phone VARCHAR(28),\n" +
-//                    "age INT);");
-//            statement.execute("insert into client values(1, 'John', 'Connor', 't1000@gmail.com', 'helloworld', '+380504556622', '24')");
-////            statement.execute("insert into contacts values(2, 'Sarah Connor')");
-////            statement.execute("insert into contacts values(3, 'Bill Clinton')");
-////            statement.execute("insert into contacts values(4, 'Barak Obama')");
-////            statement.execute("insert into contacts values(5, 'Bill Gates')");
-////            statement.execute("insert into contacts values(6, 'Linus Torvalds')");
-////            statement.execute("insert into contacts values(7, 'Steeve Jobs')");
-////            statement.execute("insert into contacts values(8, 'Alice Cooper')");
-////            statement.execute("insert into contacts values(9, 'Kenny McCormick')");
-////            statement.execute("insert into contacts values(10, 'Eric Cartmann')");
-//        } catch (SQLException e) {
-//           e.getMessage();
-//        }
-//    }
-
-//    @Test
-//    public void initialize() {
-//    }
 
     @Test
     public void getConnection() throws SQLException {
-        connection = DBManager.getConnection();
+        Connection connection = DBManager.getConnection();
         Assert.assertNotNull(connection);
     }
 
     @Test
-    public void getH2DataSource() {
+    public void getH2DataSource() throws SQLException {
+        //GIVEN:
+        String expectedUser = "sa";
+        String expectedURL = "jdbc:h2:tcp://localhost/~/test";
+        String actualUser;
+        String actualURL;
 
-    }
+        //WHEN:
+        JdbcDataSource dataSource = (JdbcDataSource) DBManager.getH2DataSource();
+        actualURL = dataSource.getURL();
+        actualUser = dataSource.getUser();
 
-    @Test
-    public void stopDb() throws SQLException {
-//        DBManager.initialize(DBManager.getH2DataSource());
-        connection = DBManager.getConnection();
-        DBManager.stopDb();
-        Assert.assertNotNull(connection);
+        //THEN:
+        Assert.assertEquals(expectedURL, actualURL);
+        Assert.assertEquals(expectedUser, actualUser);
     }
 }
